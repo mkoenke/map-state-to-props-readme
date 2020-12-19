cf2a7a4321ed4d95a9d5f59d69d89ca54ea52d16
mkoenke <mkoenke@sas.upenn.edu>
2020-12-19 11:35:42 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import shoppingListItemReducer from './reducers/shoppingListItemReducer';
-import App from './App';
-import './index.css';
+import React from "react"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import { createStore } from "redux"
+import App from "./App"
+import "./index.css"
+import shoppingListItemReducer from "./reducers/shoppingListItemReducer"
 
 const store = createStore(
   shoppingListItemReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+)
 
 ReactDOM.render(
-  <App store={store}/>,
-  document.getElementById('root')
-);
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+)
